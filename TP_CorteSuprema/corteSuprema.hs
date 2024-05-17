1fa599628ea3d98136209ee0a8a4f988c7c43831
GonzaloPurita <gpurita@frba.utn.edu.ar>
2024-05-17 18:07:47 -0300

    Terminado

--- TP_CorteSuprema/corteSuprema.hs
+++ TP_CorteSuprema/corteSuprema.hs
@@ -1,3 +1,4 @@
+import Data.List
 
 data Ley = UnaLey{
     tema :: String,
@@ -6,7 +7,7 @@ data Ley = UnaLey{
 }deriving(Show,Eq)
 
 medicinal :: Ley
-medicinal = UnaLey "Uso medicinal del cannabis " 5 ["partido cambio de todos", "sector financiero"]
+medicinal = UnaLey "Uso medicinal del cannabis" 5 ["partido cambio de todos", "sector financiero"]
 educacion :: Ley
 educacion = UnaLey "Educación superior" 30 ["docentes universitarios", "partido de centro federal"]
 tenisDeMesa :: Ley
@@ -14,12 +15,47 @@ tenisDeMesa = UnaLey "Profesionalización del tenista de mesa" 1 ["partido de ce
 tenis :: Ley
 tenis = UnaLey "tenis" 2 ["liga de deportistas autónomos"]
 
-leyesCompatibles :: Ley -> Ley -> String
-leyesCompatibles ley1 ley2 = {-- any (`elem` sectoresApoyan ley1) (sectoresApoyan ley2) && --} temaPerteneceAOtro (tema ley1) (tema ley2)    -- filter (\c -> c `elem` tema ley1) (tema ley2)
+leyesCompatibles :: Ley -> Ley -> Bool
+leyesCompatibles ley1 ley2 = any (`elem` sectoresApoyan ley1) (sectoresApoyan ley2) &&  (isInfixOf (tema ley1) (tema ley2) || isInfixOf (tema ley2) (tema ley1))
 
-temaPerteneceAOtro :: String -> String -> Bool
-temaPerteneceAOtro _ [] = False
-temaPerteneceAOtro [] _ = True
-temaPerteneceAOtro (x:strABuscar) (c:str)
-                    | x == c = 
-                    | otherwise = 
+type CriterioJuez = Ley -> Bool
+type Agenda = [String]
+agenda :: Agenda
+agenda = ["Uso medicinal del cannabis", "tenis"]
+
+opinionPublica :: CriterioJuez
+opinionPublica ley = tema ley `elem` agenda
+
+apoyoSector :: String -> CriterioJuez
+apoyoSector sector ley = sector `elem` sectoresApoyan ley
+
+presupuestoMayorA :: Int -> CriterioJuez
+presupuestoMayorA num ley = presupuesto ley > num
+
+apoyaUnSoloPartido :: String -> CriterioJuez
+apoyaUnSoloPartido partido ley = partido `elem` sectoresApoyan ley
+
+leyConstitucional :: Ley -> CriterioJuez -> Bool
+leyConstitucional ley criterio = criterio ley
+
+siempreAfirmativo :: CriterioJuez
+siempreAfirmativo _ = True
+
+leyesNoConstitucionales :: [Ley] -> [CriterioJuez] -> [Ley]
+leyesNoConstitucionales leyes jueces = filter (verificarNoConsti jueces) leyes
+
+verificarNoConsti :: [CriterioJuez] -> Ley -> Bool
+verificarNoConsti jueces ley = not (all (leyConstitucional ley) jueces)
+
+borocotizar :: [CriterioJuez] -> [CriterioJuez]
+borocotizar lista = map (\f -> not.f) lista
+
+coincidencia :: [Ley] -> CriterioJuez -> String -> Bool
+coincidencia leyes juez sector = all (verificarSiCoincide juez sector) leyes
+
+verificarSiCoincide :: CriterioJuez -> String -> Ley -> Bool
+verificarSiCoincide criterioJuez sector ley
+                    | criterioJuez ley = sector `elem` sectoresApoyan ley
+                    | otherwise = True
+
+-- Si una ley tuviera infinitos sectores que lo apoyan, podria ser declarada constitucional siempre y cuando el criterio no involucre los partidos
