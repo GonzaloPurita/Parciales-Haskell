c6476bf23c2c5d4f04367dd88b3b735f5937960e
GonzaloPurita <gpurita@frba.utn.edu.ar>
2024-05-21 21:57:59 -0300

    NoEstaDelTodoBien

--- GemaDeCristal/gemas.hs
+++ GemaDeCristal/gemas.hs
@@ -47,5 +47,41 @@ type Personalidad = Situacion -> Situacion
 
 vidente :: Personalidad
 vidente situacion = modificarSituacion "tension" (flip (-) 10) (modificarSituacion "incertidumbre" (/2) situacion)
--- relajada :: Personalidad
--- relajada situacion = 
+relajada :: Float -> Personalidad
+relajada relajamiento situacion = modificarSituacion "peligro" (relajamiento +) (modificarSituacion "tension" (flip (-) 10) situacion)
+
+-- 3)
+leGana :: Gema -> Gema -> Situacion -> Bool
+leGana gema1 gema2 situacion = (fuerza gema1 >= fuerza gema2) && situacionMejor ((personalidad gema1) situacion) ((personalidad gema2) situacion)
+
+fusion :: Situacion -> Gema -> Gema -> Gema
+fusion situacion gema1 gema2 = UnaGema (nombreFusion gema1 gema2) (fuerzaFusion gema1 gema2 situacion) (personalidadFusion gema1 gema2)
+
+nombreFusion :: Gema -> Gema -> String
+nombreFusion gema1 gema2
+                | nombre gema1 == nombre gema2 = nombre gema1
+                | otherwise = nombre gema1 ++ nombre gema2
+
+personalidadFusion :: Gema -> Gema -> Personalidad
+personalidadFusion gema1 gema2 = personalidad gema2 . personalidad gema1 . map (modificarAspecto (flip (-) 10))
+
+fuerzaFusion :: Gema -> Gema -> Situacion -> Float
+fuerzaFusion gema1 gema2 situacion
+            | situacionMejor ((personalidadFusion gema1 gema2) situacion) (personalidad gema1 situacion) && situacionMejor ((personalidadFusion gema1 gema2) situacion) (personalidad gema2 situacion) = (fuerza gema1 + fuerza gema2) * 10
+            | leGana gema1 gema2 situacion = 7 * fuerza gema1
+            | otherwise = 7 * fuerza gema2
+
+-- 5)
+fusionGrupal :: Situacion -> [Gema] -> Gema
+fusionGrupal situacion gemas = foldl1 (fusion situacion) gemas
+
+-- 6)
+
+-- foo :: (a) -> (a -> c -> Bool) -> ([c]) -> Bool
+-- foo x y z = any (== y x).z
+
+-- -- Invocaciones de la función
+-- foo 5 (+7) [1..]
+-- foo 3 even (map (< 7))
+-- foo 3 even [1, 2, 3]
+-- foo [1..] head (take 5) [1.. ]
